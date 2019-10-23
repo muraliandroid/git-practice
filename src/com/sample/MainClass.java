c015f75dd5fcf6253f4fc47faffd52a670c4c028
Deepika <deepitesting@gmail.com>
2019-10-23 23:07:11 +0530

    Two files committed

--- src/com/sample/MainClass.java
+++ src/com/sample/MainClass.java
@@ -9,6 +9,9 @@ public class MainClass {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello world!!!");
+		
+		MainClass mainClass = new MainClass();
+		mainClass.printInConsole();
 
 	}
 }
---
+++ src/com/sample/NewClass.java
@@ -0,0 +1,11 @@
+package com.sample;
+
+public class NewClass {
+
+	public static void main(String[] args) {
+		
+		System.out.println("New file");
+
+	}
+
+}
