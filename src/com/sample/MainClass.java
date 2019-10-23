6d84e68b10b007a3afedb2854d5f6280de317121
Deepika <deepitesting@gmail.com>
2019-10-23 22:16:43 +0530

    Added a method

--- src/com/sample/MainClass.java
+++ src/com/sample/MainClass.java
@@ -2,6 +2,10 @@ package com.sample;
 
 public class MainClass {
 
+	void printInConsole() {
+		System.out.println("My commit - Deepika");
+	}
+	
 	public static void main(String[] args) {
 		
 		System.out.println("Hello world!!!");
